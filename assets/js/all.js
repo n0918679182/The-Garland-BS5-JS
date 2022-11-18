3f02c3be19dcf16b2dc5cee8b5dcf75fa2b42752
wayne <n0918679182@gmail.com>
2022-11-18 15:21:43 +0800

    Update 2022-11-18T07:21:39.476Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -646,14 +646,42 @@ function memberLayoutInit() {
 
   var toMemberAccount = document.getElementById('toMemberAccount');
   var toMemberCourse = document.getElementById('toMemberCourse');
+  var toMemberAccountSm = document.getElementById('toMemberAccountSm');
+  var toMemberCourseSm = document.getElementById('toMemberCourseSm');
 
   if (memberPageCode == 1) {
     toMemberAccount.classList.add('onPage');
+    toMemberAccountSm.classList.remove('text-dark');
+    toMemberAccountSm.classList.add('text-primary');
     toMemberCourse.classList.remove('onPage');
+    toMemberCourseSm.classList.add('text-dark');
+    toMemberCourseSm.classList.remove('text-primary');
   } else if (memberPageCode == 2) {
     toMemberAccount.classList.remove('onPage');
+    toMemberAccountSm.classList.add('text-dark');
+    toMemberAccountSm.classList.remove('text-primary');
     toMemberCourse.classList.add('onPage');
+    toMemberCourseSm.classList.remove('text-dark');
+    toMemberCourseSm.classList.add('text-primary');
   }
+
+  var swiper = new Swiper(".memberNavSwiper", {
+    slidesPerView: 5,
+    spaceBetween: 10,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 5
+      },
+      992: {
+        slidesPerView: 6
+      }
+    }
+  });
 } // 登出
 
 
